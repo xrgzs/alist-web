7288556a81c33f2af9a7918db4fc5b6b03badc6c
MadDogOwner <xiaoran@xrgzs.top>
2025-03-04 08:20:59 +0800

    refactor(url): use ini to parse `.url`

--- src/pages/home/previews/url.tsx
+++ src/pages/home/previews/url.tsx
@@ -1,15 +1,25 @@
 import { MaybeLoading } from "~/components"
+import { recordKeysToLowerCase } from "~/utils"
 import { FileInfo } from "./info"
 import { useFetchText, useParseText, useT } from "~/hooks"
 import { createEffect } from "solid-js"
 import { Button } from "@hope-ui/solid"
+import { parse } from "ini"
 
 export default function () {
   const [content] = useFetchText()
   function openInNewWindow() {
-    const url = content()?.content
-    const { text } = useParseText(url)
-    url && window.open(text(), "_blank")
+    const ini = content()?.content
+    const { text } = useParseText(ini)
+    const config = recordKeysToLowerCase(parse(text()))
+    const url = config.internetshortcut?.url
+    if (url) {
+      const a = document.createElement("a")
+      a.href = url
+      a.rel = "noopener noreferrer"
+      a.target = "_blank"
+      a.click()
+    }
   }
   createEffect(() => {
     openInNewWindow()
